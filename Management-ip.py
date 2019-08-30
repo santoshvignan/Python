93423b13d0c276713c1c6a4574cf3492b962c2ba
santoshvignan <53837716+santoshvignan@users.noreply.github.com>
2019-08-30 12:35:53 -0400

    Create Management-ip.py

---
+++ Management-ip.py
@@ -0,0 +1,13 @@
+import os
+reachablehosts = []
+unreachablehosts = []
+for ip in range(1,10):
+    host = '29.29.29.'+ str(ip)
+    print "Currently pinging the following ip" + " " + host
+    response = os.system("ping -c 5 " + host + ">/dev/null")
+    if response == 0:
+        reachablehosts.append(host)
+    else:
+        unreachablehosts.append(host)
+
+print "Reachable hosts are: " +  reachablehosts
