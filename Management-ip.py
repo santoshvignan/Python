3cf6e9676b2cb68152094e6a1ea182da89686200
m.santoshvignan@gmail.com <m.santoshvignan@gmail.com>
2019-08-30 13:58:15 -0400

    Commit message: Checking 110 IP addresses

--- Management-ip.py
+++ Management-ip.py
@@ -1,7 +1,7 @@
 import os
 reachablehosts = []
 unreachablehosts = []
-for ip in range(1,10):
+for ip in range(1,110):
     host = '29.29.29.'+ str(ip)
     print "Currently pinging the following ip" + " " + host
     response = os.system("ping -c 5 " + host + ">/dev/null")
